2803e49a827c171c84fda298c992d7f8cb75688b
AnjaliShah_71 <anjalishah2823@gmail.com>
2026-09-11 19:48:58 +0530

    calculator.dart

--- calculator.dart
+++ calculator.dart
@@ -28,9 +28,9 @@ class Cal extends State<Calculator>{
           ),
           borderRadius: BorderRadius.circular(30),
           gradient: LinearGradient(
-         // colors: [Colors.cyan,Colors.white10],
-         colors:[Color(0xff6a11cb),Color(0xff2575fc),Color(0xff8E2DE2)],
-           // colors:[Color(0xff0F2027),Color(0xff2C5364),Color(0xff203A43)],
+         
+         colors:[Color(#5FB1C8),Color(#356AA0),Color(#356AA0)],
+           
            begin: Alignment.topCenter,
           ),
         ),
